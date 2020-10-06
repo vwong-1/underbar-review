897d8ed75e7b27639a85ada0730473e9c6e04430
Victor Wong <victorw1992@gmail.com>
2020-10-06 15:46:29 -0700

    Implemented map, pluck

--- src/underbar.js
+++ src/underbar.js
@@ -126,6 +126,13 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    let result = [];
+
+    _.each(collection, function(value, key, collection) {
+      result.push(iterator(value, key, collection));
+    });
+
+    return result;
   };
 
   /*
