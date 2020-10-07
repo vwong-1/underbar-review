e7135664038cc1ef561819044e8d2f0a9d2922ee
Victor Wong <victorw1992@gmail.com>
2020-10-06 21:05:00 -0700

    Implemented extend, defaults

--- spec/part2.js
+++ spec/part2.js
@@ -183,9 +183,11 @@
       });
 
       it('should override properties found on the destination', function() {
-        // Replace this line with an `expect` statement that tests
-        // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var destination = { x: '2' };
+        var source = { x: '5' };
+        var extended = _.extend(destination, source);
+
+        expect(extended.x).to.equal('5');
       });
 
       it('should not override properties not found in the source', function() {
@@ -261,9 +263,14 @@
       });
 
       it('should copy any property whose key is not already set on the target', function() {
-        // Replace this line with an `expect` statement that tests
-        // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var destination = {};
+        var source = { a: 100, b: 200, c: 300 };
+
+        _.defaults(destination, source);
+
+        expect(destination.a).to.equal(100);
+        expect(destination.b).to.equal(200);
+        expect(destination.c).to.equal(300);
       });
 
       it('should not copy a property if that key is already set on the target', function() {
--- src/underbar.js
+++ src/underbar.js
@@ -243,11 +243,25 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (let i = 1; i < arguments.length; i++) {
+      for (const key in arguments[i]) {
+        obj[key] = arguments[i][key];
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for (let i = 1; i < arguments.length; i++) {
+      for (const key in arguments[i]) {
+        if (obj[key] === undefined) {
+          obj[key] = arguments[i][key];
+        }
+      }
+    }
+    return obj;
   };
 
 
